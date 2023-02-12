0d752992b3256ef6b63e7c3872f90b40159bd48f
Hatim aka geoaxis <72706+geoaxis@users.noreply.github.com>
2023-02-12 23:41:20 +0100

    Inital import

---
+++ pages/api/hello.ts
@@ -0,0 +1,51 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import mjml2html from 'mjml'
+
+
+
+interface Data {
+  output: Html | Error,
+}
+
+interface Html {
+  html: string
+}
+
+interface Error {
+  component: string
+  error: any
+}
+
+
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+
+  if (req.method === 'POST') {
+    let templatedMjml= '';
+  const Handlebars = require("handlebars");
+  try {
+    const template = Handlebars.compile(req.body.mjml);
+    templatedMjml = template(req.body.params);
+
+  } catch (error) {
+    res.status(400).json({output: { component: "handlesbar" ,"error" :error }})
+  }
+
+  let mjmlResponse;
+  try {
+    mjmlResponse = mjml2html(templatedMjml, { validationLevel: 'strict' })
+  } catch (error) {
+    res.status(400).json({output: { component: "mjmml" ,"error" :error}})
+  }
+
+    res.status(200).json({output: mjmlResponse})
+  } else {
+    res.status(501);
+  }
+
+
+}
---
+++ pages/index.tsx
@@ -0,0 +1,106 @@
+import CodeMirror from '@uiw/react-codemirror';
+import { xml } from '@codemirror/lang-xml';
+import { json } from '@codemirror/lang-json';
+import { useState, useEffect, useCallback } from 'react'
+import '@aws-amplify/ui-react/styles.css';
+
+
+import {
+  HorizontalPageSplit,
+  VerticalPageSplit
+} from 'react-page-split';
+import 'react-page-split/style.css';
+
+
+
+interface Data {
+  output: Html | Error,
+}
+
+interface Html {
+  html: string
+}
+
+interface Error {
+  component: string
+  error: any
+}
+
+
+
+function Home() {
+
+
+  const handleClick = async () => {
+
+    console.log(typeof(jsonInput));
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        mjml: input, params:
+          JSON.parse(jsonInput)
+      })
+    };
+    try {
+      const data = await (await fetch('/api/hello', requestOptions)).json()
+      setHtml(data.output.html)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const [input, setInput] = useState(`<mjml>
+  <mj-body>
+    <mj-section>
+      <mj-column>
+      {{#each people}}
+      <mj-text font-size="20px" color="#F45E43">{{this}}</mj-text>
+    {{/each}}
+      </mj-column>
+    </mj-section>
+  </mj-body>
+</mjml>
+`)
+
+const [jsonInput, setJsonInput] = useState('{ "people": ["a", "b"] }')
+
+  const [html, setHtml] = useState(`
+<p>None</p>
+`);
+
+
+  const onChange = useCallback((value, viewUpdate) => {
+    setInput(value);
+  }, []);
+
+  const onChangeJson = useCallback((value, viewUpdate) => {
+    setJsonInput(value);
+  }, []);
+
+  return (
+
+    <HorizontalPageSplit widths={['50%', '50%']}>
+            <VerticalPageSplit>
+            <CodeMirror
+            onChange={onChange}
+            value={input}
+            extensions={[xml()]}
+          />
+
+          <CodeMirror
+            onChange={onChangeJson}
+            value={jsonInput}
+            extensions={[json()]}
+          />
+          <button onClick={handleClick}>Preview</button>
+            </VerticalPageSplit>
+            <VerticalPageSplit heights={['50%', '50%']}>
+            <div dangerouslySetInnerHTML={{ __html: html }} />
+
+            </VerticalPageSplit>
+        </HorizontalPageSplit>
+  );
+}
+export default Home;
